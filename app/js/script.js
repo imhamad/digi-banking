b0d5e6e6088090e9b9f1c9aeac72b61250339730
Hamad <hamad.coder@gmail.com>
2022-06-20 11:13:38 +0500

    [css| added]

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-// alert('hello');
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click',function(){
+console.log('clicked');
+if(btnHamburger.classList.contains('open')){
+   btnHamburger.classList.remove('open');
+}
+else{
+   btnHamburger.classList.add('open');
+}
+});
